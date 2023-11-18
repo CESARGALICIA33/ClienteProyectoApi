d4271ac79a3d5b0c734b799776cbbb09a4d8a8df
Cesar Paolo <galiciacesar21@gmail.com>
2023-11-18 12:45:59 -0600

    V2.4C

---
+++ js/eliminar-script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const eliminarBtn = document.getElementById('eliminarBtn');
+
+    eliminarBtn.addEventListener('click', function () {
+        // Obtener el iduser del localStorage
+        const Iduser = localStorage.getItem('userId');
+        console.log(localStorage.getItem('userId'));
+        if (Iduser) {
+            // Llamar a la función para eliminar el usuario en ambas APIs
+            eliminarUsuarioEnAmbasAPIs(Iduser);
+        } else {
+            console.error('Error: No se encontró el iduser en el localStorage');
+        }
+    });
+
+    async function eliminarUsuarioEnAmbasAPIs(Iduser) {
+        try {
+            // Realizar solicitud a la primera API para eliminar el usuario
+            const responseApi1 = await fetch('http://localhost:82/APIproyectofinal/api-rest/EliminarCorreo.php/', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    Iduser: Iduser,
+                }),
+            });
+
+            // Realizar solicitud a la segunda API para eliminar el usuario
+            const responseApi2 = await fetch('http://localhost:82/APIproyectofinal/api-rest/EliminarCandidato.php', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    Iduser: Iduser,
+                }),
+            });
+
+            // Verificar el resultado de ambas solicitudes
+            if (responseApi1.ok && responseApi2.ok) {
+                console.log('Usuario eliminado correctamente en ambas APIs');
+            } else {
+                console.error('Error al eliminar el usuario en al menos una de las APIs');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+});
