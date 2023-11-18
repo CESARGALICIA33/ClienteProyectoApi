d08b55fb8d1fffb6a89d95d42f79d151ac1baf68
Cesar Paolo <galiciacesar21@gmail.com>
2023-11-18 09:05:56 -0600

    VC2.3

--- js/login-script.js
+++ js/login-script.js
@@ -41,7 +41,7 @@ async function submitForm(event) {
                     if (accountType === '1') {
                         window.location.href = 'Index.html'; // Cambiar a la URL correcta
                     } else if (accountType === '2') {
-                        window.location.href = 'pagina2.html'; // Cambiar a la URL correcta
+                        window.location.href = 'vistaEmpresa.html'; // Cambiar a la URL correcta
                     }
                
             } else {
